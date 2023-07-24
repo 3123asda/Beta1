9059562fd4ceba7fa0ffff183a949e136f105376
admin <weakhero2@gmail.com>
2023-07-24 15:24:39 +0800

    first commit

---
+++ src/pages/secondPages.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+function secondPages() {
+  return (
+    <div>
+        <p>Go to: <Link to="/">First Page</Link></p>
+    </div>
+  )
+}
+
+export default secondPages
